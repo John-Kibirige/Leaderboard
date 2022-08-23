841cb9de4d33ddd30b5095416ad348069d88d03b
John Kibirige <kibirigejohn005@gmail.com>
2022-08-23 16:28:00 +0300

    Render all name-score items from a list to the ui

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './styles/main.scss';
 import scores from './modules/scores.js';
+import { createScore } from './modules/create-score';
+
+const displayAllScores = document.querySelector('.display-all-scores');
+scores.forEach((scoreObj) => {
+  displayAllScores.appendChild(createScore(scoreObj));
+});
