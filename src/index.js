d49bff6ef6fdc5aeb533905af24f57fe47422cda
John Kibirige <kibirigejohn005@gmail.com>
2022-08-24 17:41:41 +0300

    create a new game, called kipi and get its unique id

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import './styles/main.scss';
 import scores from './modules/scores.js';
 import createScore from './modules/create-score.js';
+import getIdentifier from './modules/api.js';
 
 const displayAllScores = document.querySelector('.display-all-scores');
 scores.forEach((scoreObj) => {
   displayAllScores.appendChild(createScore(scoreObj));
+});
+
+// get exact id
+getIdentifier().then((response) => {
+  const { result } = response;
+  const [, , , actualId] = result.split(' ');
 });
