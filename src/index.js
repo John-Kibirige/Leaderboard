879788df0b1e0015debe6c7a39eed3767b223190
John Kibirige <kibirigejohn005@gmail.com>
2022-08-25 12:48:47 +0300

    Implement the post method for storing user form data

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import './styles/main.scss';
 import scores from './modules/scores.js';
-import createScore from './modules/create-score.js';
-import getIdentifier from './modules/api.js';
+import { createScore, Score } from './modules/create-score.js';
+import { postData } from './modules/api.js';
+
+const submit = document.querySelector('#submit-btn');
+const name = document.querySelector('.form-name').value;
+let score = document.querySelector('.form-score').value;
 
 const displayAllScores = document.querySelector('.display-all-scores');
 scores.forEach((scoreObj) => {
   displayAllScores.appendChild(createScore(scoreObj));
 });
 
-// get exact id
-getIdentifier().then((response) => {
-  const { result } = response;
-  const [, , , actualId] = result.split(' ');
+submit.addEventListener('click', () => {
+  score = +score;
+  const newScore = new Score(name, score);
+  postData(newScore).then((returned) => {
+    if (returned.result === 'Leaderboard score created correctly.') {
+      // display success message
+    } else {
+      // display failure message
+    }
+  });
 });
--- src/modules/api.js
+++ src/modules/api.js
@@ -1,17 +1,16 @@
-const getIdentifier = async () => {
+const postData = async (scoreObject) => {
   const response = await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/',
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/2CK77ljZH2u23q36Lc0c/scores/',
     {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        Accept: 'application/json',
       },
-      body: JSON.stringify({
-        name: 'Kipi game',
-      }),
+      body: JSON.stringify(scoreObject),
     }
   );
   return response.json();
 };
 
-export default getIdentifier;
+export { postData };
